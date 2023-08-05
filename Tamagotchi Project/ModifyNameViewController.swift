151a988619b2a0c2deeec8144cd1a6e58c1322f7
limsub <limsss9076@naver.com>
2023-08-05 23:01:14 +0900

    이름 변경 기능

--- Tamagotchi Project/ModifyNameViewController.swift
+++ Tamagotchi Project/ModifyNameViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ModifyNameViewController: UIViewController {
+    
+    let placeholderText = "대장님 이름을 알려주세요!"
+    let pastName = UserDefaults.standard.string(forKey: "userName")
 
+    @IBOutlet var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = TamaColor.back.tcolor
 
-       
+        title = "대장님 이름 정하기"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "저장",
+            style: .plain,
+            target: self,
+            action: #selector(saveButtonTapped)
+        )
+        navigationItem.titleView?.tintColor = TamaColor.back.tcolor
+        
+        nameTextField.borderStyle = .none
+        nameTextField.layer.addBorder([.bottom], color: TamaColor.font.tcolor, width: 1)
+        nameTextField.backgroundColor = .clear
+        nameTextField.placeholder = placeholderText
+        
+        nameTextField.text = pastName
     }
     
-
+    @objc
+    func saveButtonTapped(_ sender: UIButton) {
+        if let newName = nameTextField.text {
+            if (newName.count >= 2 && newName.count <= 6) {
+                UserDefaults.standard.set(newName, forKey: "userName")
+                navigationController?.popViewController(animated: true)
+            }
+            else {
+                let alert = UIAlertController(title: "글자 수 오류", message: "대장 이름은 2글자 이상 6글자 이하까지 가능합니다", preferredStyle: .alert)
+                
+                let ok = UIAlertAction(title: "확인", style: .default)
+                
+                alert.addAction(ok)
+                
+                present(alert, animated: true)
+            }
+        }
+    }
+    
+    @IBAction func returnTapped(_ sender: UITextField) {
+    }
+    
+    
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    
+    
 }
